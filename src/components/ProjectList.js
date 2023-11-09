3af18dd2f22d2f56d419e739d84db514af8c6237
asmaali2 <asma.maalim7@gmail.com>
2023-11-09 15:20:34 +0300

    done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -7,8 +7,13 @@ function ProjectList({ projects }) {
     <div id="projects">
       <h2>My Projects</h2>
       <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project)=>(
+           <ProjectItem key={project.id} name={project.name}  technologies={project.technologies} />
+        ))}
+      </div>
     </div>
   );
 }
-
-export default ProjectList;
+export default ProjectList
+  
